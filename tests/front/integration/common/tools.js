224c39513f67c876b402e75d8e7350d561f289c4
Tamara Robichet <tamara@robichet.com>
2019-08-26 11:47:29 +0200

    Fetch contracts in tools

--- tests/front/integration/common/tools.js
+++ tests/front/integration/common/tools.js
@@ -1,10 +1,18 @@
+const CONTRACT_PATH = `${process.cwd()}/tests/front/integration/common/contracts/`
+const { readFileSync } = require('fs')
+
 function mockRequests(page, answers) {
   page.on('request', req => {
     const requestUrl = req.url();
-    const answer = answers[requestUrl]
-    if (!answer) return req.continue();
-    const { body, contentType } = answer
-    req.respond({ contentType, body });
+    const contract = answers[requestUrl]
+    if (!contract) return req.continue();
+
+    try {
+      const body = readFileSync(`${CONTRACT_PATH}${contract}`)
+      req.respond({ contentType: 'application/json', body });
+    } catch (e) {
+      throw Error(`Can't find contract at ${CONTRACT_PATH}${contract}`)
+    }
   });
 }
 
--- tests/front/integration/pimenrich/filters/number-filter.integration.js
+++ tests/front/integration/pimenrich/filters/number-filter.integration.js
@@ -1,24 +1,18 @@
 const process = require('process')
 const fs = require('fs')
 const tools = require('../../common/tools');
-const datagridLoad = fs.readFileSync(`${process.cwd()}/tests/front/integration/common/contracts/product_grid.json`, 'utf-8');
-const loadProducts = fs.readFileSync(`${process.cwd()}/tests/front/integration/common/contracts/load_products.json`, 'utf-8');
-const categoryChildren = fs.readFileSync(`${process.cwd()}/tests/front/integration/common/contracts/category_children.json`, 'utf-8');
-const listTree = fs.readFileSync(`${process.cwd()}/tests/front/integration/common/contracts/list_tree.json`, 'utf-8');
-const attributesFilters = fs.readFileSync(`${process.cwd()}/tests/front/integration/common/contracts/attributes_filters.json`, 'utf-8');
 
 const renderProductGrid = async (page) => {
-  const answerJSON = (body) => { return { contentType: 'application/json', body}}
   tools.mockRequests(page, {
-    'http://pim.com/datagrid_view/rest/product-grid/default': answerJSON(JSON.stringify({view: null})),
-    'http://pim.com/datagrid_view/rest/product-grid/default-columns': answerJSON(JSON.stringify(["identifier","image","label","family","enabled","completeness","created","updated","complete_variant_products"])),
-    'http://pim.com/enrich/product-category-tree/product-grid/children.json?dataLocale=undefined&context=view&id=0&select_node_id=-2&with_items_count=1&include_sub=1': answerJSON(categoryChildren),
-    'http://pim.com/datagrid/product-grid/load?dataLocale=en_US&params%5BdataLocale%5D=en_US&product-grid%5B_parameters%5D%5Bview%5D%5Bcolumns%5D=identifier%2Cimage%2Clabel%2Cfamily%2Cenabled%2Ccompleteness%2Ccreated%2Cupdated%2Ccomplete_variant_products%2Csuccess%2C%5Bobject+Object%5D': answerJSON(datagridLoad),
-    'http://pim.com/datagrid/product-grid/attributes-filters?page=1&locale=en_US': answerJSON(attributesFilters),
-    'http://pim.com/enrich/product-category-tree/product-grid/list-tree.json?dataLocale=undefined&select_node_id=0&include_sub=1&context=view': answerJSON(listTree),
-    'http://pim.com/enrich/product-category-tree/product-grid/children.json?dataLocale=undefined&context=view&id=1&select_node_id=-2&with_items_count=1&include_sub=1': answerJSON(categoryChildren),
-    'http://pim.com/datagrid/product-grid?dataLocale=en_US&product-grid%5B_pager%5D%5B_page%5D=1&product-grid%5B_pager%5D%5B_per_page%5D=25&product-grid%5B_parameters%5D%5Bview%5D%5Bcolumns%5D=identifier%2Cimage%2Clabel%2Cfamily%2Cenabled%2Ccompleteness%2Ccreated%2Cupdated%2Ccomplete_variant_products%2Csuccess%2C%5Bobject%20Object%5D&product-grid%5B_parameters%5D%5Bview%5D%5Bid%5D=&product-grid%5B_sort_by%5D%5Bupdated%5D=DESC&product-grid%5B_filter%5D%5Bscope%5D%5Bvalue%5D=ecommerce&product-grid%5B_filter%5D%5Bcategory%5D%5Bvalue%5D%5BtreeId%5D=1&product-grid%5B_filter%5D%5Bcategory%5D%5Bvalue%5D%5BcategoryId%5D=-2&product-grid%5B_filter%5D%5Bcategory%5D%5Btype%5D=1': answerJSON(loadProducts),
-    'http://pim.com/datagrid/product-grid?dataLocale=en_US&product-grid%5B_pager%5D%5B_page%5D=1&product-grid%5B_pager%5D%5B_per_page%5D=25&product-grid%5B_parameters%5D%5Bview%5D%5Bcolumns%5D=identifier%2Cimage%2Clabel%2Cfamily%2Cenabled%2Ccompleteness%2Ccreated%2Cupdated%2Ccomplete_variant_products%2Csuccess%2C%5Bobject%20Object%5D&product-grid%5B_parameters%5D%5Bview%5D%5Bid%5D=&product-grid%5B_sort_by%5D%5Bupdated%5D=DESC&product-grid%5B_filter%5D%5Bscope%5D%5Bvalue%5D=ecommerce&product-grid%5B_filter%5D%5Bweight%5D%5Bvalue%5D=&product-grid%5B_filter%5D%5Bweight%5D%5Btype%5D=empty&product-grid%5B_filter%5D%5Bweight%5D%5Bunit%5D=MILLIGRAM&product-grid%5B_filter%5D%5Bcategory%5D%5Bvalue%5D%5BtreeId%5D=1&product-grid%5B_filter%5D%5Bcategory%5D%5Bvalue%5D%5BcategoryId%5D=-2&product-grid%5B_filter%5D%5Bcategory%5D%5Btype%5D=1': answerJSON(loadProducts)
+    'http://pim.com/datagrid_view/rest/product-grid/default': 'product-grid/default_views.json',
+    'http://pim.com/datagrid_view/rest/product-grid/default-columns': 'product-grid/default_columns.json',
+    'http://pim.com/enrich/product-category-tree/product-grid/children.json?dataLocale=undefined&context=view&id=0&select_node_id=-2&with_items_count=1&include_sub=1': 'product-grid/category_children.json',
+    'http://pim.com/datagrid/product-grid/load?dataLocale=en_US&params%5BdataLocale%5D=en_US&product-grid%5B_parameters%5D%5Bview%5D%5Bcolumns%5D=identifier%2Cimage%2Clabel%2Cfamily%2Cenabled%2Ccompleteness%2Ccreated%2Cupdated%2Ccomplete_variant_products%2Csuccess%2C%5Bobject+Object%5D': 'product-grid/product_grid.json',
+    'http://pim.com/datagrid/product-grid/attributes-filters?page=1&locale=en_US': 'product-grid/attributes_filters.json',
+    'http://pim.com/enrich/product-category-tree/product-grid/list-tree.json?dataLocale=undefined&select_node_id=0&include_sub=1&context=view': 'product-grid/list_tree.json',
+    'http://pim.com/enrich/product-category-tree/product-grid/children.json?dataLocale=undefined&context=view&id=1&select_node_id=-2&with_items_count=1&include_sub=1': 'product-grid/category_children.json',
+    'http://pim.com/datagrid/product-grid?dataLocale=en_US&product-grid%5B_pager%5D%5B_page%5D=1&product-grid%5B_pager%5D%5B_per_page%5D=25&product-grid%5B_parameters%5D%5Bview%5D%5Bcolumns%5D=identifier%2Cimage%2Clabel%2Cfamily%2Cenabled%2Ccompleteness%2Ccreated%2Cupdated%2Ccomplete_variant_products%2Csuccess%2C%5Bobject%20Object%5D&product-grid%5B_parameters%5D%5Bview%5D%5Bid%5D=&product-grid%5B_sort_by%5D%5Bupdated%5D=DESC&product-grid%5B_filter%5D%5Bscope%5D%5Bvalue%5D=ecommerce&product-grid%5B_filter%5D%5Bcategory%5D%5Bvalue%5D%5BtreeId%5D=1&product-grid%5B_filter%5D%5Bcategory%5D%5Bvalue%5D%5BcategoryId%5D=-2&product-grid%5B_filter%5D%5Bcategory%5D%5Btype%5D=1': 'product-grid/load_products.json',
+    'http://pim.com/datagrid/product-grid?dataLocale=en_US&product-grid%5B_pager%5D%5B_page%5D=1&product-grid%5B_pager%5D%5B_per_page%5D=25&product-grid%5B_parameters%5D%5Bview%5D%5Bcolumns%5D=identifier%2Cimage%2Clabel%2Cfamily%2Cenabled%2Ccompleteness%2Ccreated%2Cupdated%2Ccomplete_variant_products%2Csuccess%2C%5Bobject%20Object%5D&product-grid%5B_parameters%5D%5Bview%5D%5Bid%5D=&product-grid%5B_sort_by%5D%5Bupdated%5D=DESC&product-grid%5B_filter%5D%5Bscope%5D%5Bvalue%5D=ecommerce&product-grid%5B_filter%5D%5Bweight%5D%5Bvalue%5D=&product-grid%5B_filter%5D%5Bweight%5D%5Btype%5D=empty&product-grid%5B_filter%5D%5Bweight%5D%5Bunit%5D=MILLIGRAM&product-grid%5B_filter%5D%5Bcategory%5D%5Bvalue%5D%5BtreeId%5D=1&product-grid%5B_filter%5D%5Bcategory%5D%5Bvalue%5D%5BcategoryId%5D=-2&product-grid%5B_filter%5D%5Bcategory%5D%5Btype%5D=1': 'product-grid/load_products.json',
   });
 
   return page.evaluate(({data, extension}) => {
@@ -40,7 +34,7 @@ describe('Product grid > number filter', () => {
     try {
      await renderProductGrid(page)
     } catch (e) {
-      console.log("Error", e)
+      throw Error(e.message)
     }
   }, 60000);
 
